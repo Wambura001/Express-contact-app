d617c59a97d1a0c82cb267bb5a058017ce00660e
wambura <meshack.sirincha@student.moringaschool.com>
2024-01-26 11:49:04 +0900

    connect to mondo db

--- mycontacts_backend/controller/contactController.js
+++ mycontacts_backend/controller/contactController.js
@@ -1,16 +1,17 @@
-// @desc Get all contacts
-// @route GET /api/contacts
-// @access Public
+const asyncHandler = require("express-async-handler")
+    // @desc Get all contacts
+    // @route GET /api/contacts
+    // @access Public
 
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async(req, res) => {
     res.status(200).json({ message: "Get all contacts" });
-}
+});
 
 // @desc Create New contact
 // @route POST /api/contacts
 // @access Public
 
-const postContact = (req, res) => {
+const postContact = asyncHandler(async(req, res) => {
     console.log('The request body is :', req.body)
     const { name, phone, email } = req.body
     if (!name || !phone || !email) {
@@ -18,30 +19,30 @@ const postContact = (req, res) => {
         throw new Error("Please enter all fields!");
     }
     res.status(201).json({ message: "Create contact" });
-}
+});
 
 // @desc Get contact
 // @route GET /api/contacts/:id
 // @access Public
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Get contact for ${req.params.id}` });
-}
+});
 
 // @desc Update contact
 // @route PUT /api/contacts/:id
 // @access Public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Update contact for ${req.params.id}` });
-}
+});
 
 // @desc Delete contact
 // @route DELETE /api/contacts/:id
 // @access Public
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Delete contact for ${req.params.id}` });
-}
+});
 
 
 module.exports = { getContacts, postContact, getContact, deleteContact, updateContact }
